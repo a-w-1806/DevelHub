af4ab965f8ede8845656e6be7d26e41d12802f8c
Yuchen Wang <yuchenwang.sh@gmail.com>
2019-02-01 17:41:38 +0800

    Routes added.

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
 const app = express();
 
 const db = require("./config/keys").mongoURI;
@@ -13,6 +17,10 @@ mongoose
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
+
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
 
