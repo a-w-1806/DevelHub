3bd31e988ee34a3cdff5e24c5901b26323abf432
Yuchen Wang <yuchenwang.sh@gmail.com>
2019-02-01 20:30:47 +0800

    users/register

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,4 +1,9 @@
 const express = require("express");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+
+const User = require("../../Models/User");
+
 const router = express.Router();
 
 router.get("/test", (req, res) =>
@@ -6,5 +11,45 @@ router.get("/test", (req, res) =>
     msg: "Users works"
   })
 );
+
+// @route   POST /api/users/register
+// @desc    Register user
+// @access  Public
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      // This email already exists.
+      res.status(400).json({ email: "Email already exists" });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: "200", // Size
+        r: "pg", // Rating
+        d: "mm" // Default
+      });
+
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+      bcrypt.genSalt(10, (err, salt) => {
+        if (err) {
+          throw err;
+        }
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) {
+            throw err;
+          }
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
 
 module.exports = router;
