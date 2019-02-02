9e991d8006bbc81875eb0350a42a7916a6387c95
Yuchen Wang <yuchenwang.sh@gmail.com>
2019-02-02 15:28:53 +0800

    Route to check current user's profile.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,37 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
+
+const Profile = require("../../Models/Profile");
 
 router.get("/test", (req, res) =>
   res.json({
     msg: "Profile works"
   })
+);
+
+//  @route  GET api/profile
+//  @desc   Get the current users profile
+//  @access Private
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = "There is no profile for this user.";
+          return res.status(404).json(errors);
+        }
+        return res.json(profile);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(404).json(err);
+      });
+  }
 );
 
 module.exports = router;
