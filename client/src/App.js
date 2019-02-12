574784a2e0ab356b9f201ea3f4c0adbd70e93665
Yuchen Wang <yuchenwang.sh@gmail.com>
2019-02-12 20:45:47 +0800

    Logout completed. Expiration checked.

--- client/src/App.js
+++ client/src/App.js
@@ -22,6 +22,16 @@ if (localStorage.jwtToken) {
   const decoded = jwt_decode(localStorage.jwtToken);
   // Set user and isAuthenticated
   store.dispatch(setCurrentUser(decoded));
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // TODO: Clear current Profile
+    // Redirect to login
+    window.location.href = "/login";
+  }
 }
 
 class App extends Component {
