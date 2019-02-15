82641c7e5cd0b62e041297938ad212632579a1f9
Yuchen Wang <yuchenwang.sh@gmail.com>
2019-02-16 06:58:16 +0800

    Experience panel on dashboard. Delete experience.

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -5,6 +5,7 @@ import { connect } from "react-redux";
 import { getCurrentProfile, deleteAccount } from "../../actions/profileActions";
 import Spinner from "../common/Spinner";
 import ProfileActions from "./ProfileActions";
+import Experience from "./Experience";
 
 class Dashboard extends Component {
   componentDidMount() {
@@ -35,6 +36,7 @@ class Dashboard extends Component {
             </p>
             <ProfileActions />
             {/* TODO: exp and edu */}
+            <Experience experience={profile.experience} />
             <div style={{ marginBottom: "60px" }}>
               <button
                 onClick={this.onDeleteClick.bind(this)}
