abad82974b4b1bb16337a2b37154a82db3e0d895
Yuchen Wang <yuchenwang.sh@gmail.com>
2019-02-17 16:56:39 +0800

    Route for posts and input field completed.

---
+++ client/src/components/posts/Posts.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PostForm from "./PostForm";
+
+class Posts extends Component {
+  render() {
+    return (
+      <div className="feed">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <PostForm />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default connect()(Posts);
