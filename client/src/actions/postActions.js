8e5df2003f3c890a9b9bb0bdd1470524b13c0a78
Yuchen Wang <yuchenwang.sh@gmail.com>
2019-02-17 17:17:24 +0800

    Redux for getting posts.

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { ADD_POST, GET_ERRORS } from "./types";
+import { ADD_POST, GET_ERRORS, POST_LOADING, GET_POSTS } from "./types";
 
 export const addPost = postData => dispatch => {
   axios
@@ -14,6 +14,24 @@ export const addPost = postData => dispatch => {
       dispatch({
         type: GET_ERRORS,
         payload: err.response.data
+      })
+    );
+};
+
+export const getPosts = () => dispatch => {
+  dispatch({ type: POST_LOADING });
+  axios
+    .get("/api/posts")
+    .then(res =>
+      dispatch({
+        type: GET_POSTS,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_POSTS,
+        payload: null
       })
     );
 };
--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,9 +1,26 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import PostForm from "./PostForm";
+import { getPosts } from "../../actions/postActions";
+import Spinner from "../common/Spinner";
+import PostFeed from "./PostFeed";
 
 class Posts extends Component {
+  componentDidMount() {
+    this.props.getPosts();
+  }
+
   render() {
+    const { posts, loading } = this.props.post;
+    let postContent;
+
+    if (loading) {
+      postContent = <Spinner />;
+    } else {
+      postContent = <PostFeed posts={posts} />;
+    }
+
     return (
       <div className="feed">
         <div className="container">
@@ -18,4 +35,16 @@ class Posts extends Component {
   }
 }
 
-export default connect()(Posts);
+Posts.propTypes = {
+  getPosts: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  post: state.post
+});
+
+export default connect(
+  mapStateToProps,
+  { getPosts }
+)(Posts);
